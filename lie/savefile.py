8bdb620b0f8c259c138da6277012de60196bea5c
megawidget <megawidget@gmail.com>
2011-03-19 20:42:25 -0700

    pushed context into LIE, added initial savefile functionality

---
+++ lie/savefile.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+import tarfile
+import cPickle
+import os, shutil
+import globals
+
+FILE_EXT = '.tar.bz2'
+WRITE_MODE = 'w:bz2'
+READ_MODE = 'r:bz2'
+
+class SaveFile(object):
+    """Saves and loads game data."""
+    def __init__(self, filename):
+        assert(filename is not None)
+        self._filename=filename
+    
+    def load(self):
+        savedir = globals.savefile_location+self._filename
+        savefile = savedir+FILE_EXT
+        result = {}
+        assert(not os.access(savedir, os.F_OK))
+        if not os.access(savefile, os.F_OK):
+            return None
+        tar = tarfile.open(savefile, READ_MODE)
+        tar.extractall(globals.savefile_location)
+        pickles = os.listdir(savedir)
+        print pickles
+        assert(len(pickles)>0)
+        for pickle in pickles:
+            assert(pickle[-7:]=='.pickle')
+            with open(savedir+'/'+pickle, 'r') as f:
+                result[pickle[:-7]]=cPickle.load(f)
+        shutil.rmtree(savedir)
+        os.remove(savefile)
+        return result
+
+    def save(self, candidates):
+        savedir = globals.savefile_location+self._filename
+        savefile = savedir+FILE_EXT
+        assert(not os.access(savedir, os.F_OK))
+        os.mkdir(savedir)
+        tar = tarfile.open(savefile, WRITE_MODE)
+        for key,value in candidates.items():
+            filename=savedir+'/'+key+'.pickle'
+            with open(filename, 'w') as f:
+                cPickle.dump(value, f)
+            tar.add(filename)
+            os.remove(filename)
+        shutil.rmtree(savedir)
+        tar.close()
+        return
+
+if __name__ == '__main__':
+    import unittest
+
+    class SaveFileTest(unittest.TestCase):
+        """Test SaveFile functionality."""
+
+        def setUp(self):
+            globals.savefile_location='./'
+
+        def test01_save_and_load(self):
+            """Testing saving and loading"""
+            data_in={'one':1, 'two':2, 'tuple':(True,False,None)}
+            savefile=SaveFile('testsave')
+            savefile.save(data_in)
+            data_out=savefile.load()
+            self.assertEqual(data_in, data_out)
+    
+    suite = unittest.TestLoader().loadTestsFromTestCase(SaveFileTest)
+    unittest.TextTestRunner(verbosity=2).run(suite)
